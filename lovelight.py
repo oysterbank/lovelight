531d2662b21b71557618f816a61ecae9b7030cc1
Kris Laratta <krislaratta@gmail.com>
2019-12-07 19:44:06 -0800

    Add graceful_stop context manager to allow user to define a state for routines to end on after a keyboard interrupt.

--- lovelight.py
+++ lovelight.py
@@ -1,10 +1,11 @@
+from contextlib import contextmanager
 from phue import Bridge
 
 __author__ = "Kris Laratta"
 __copyright__ = "Copyright (C) 2019 Kris Laratta"
 __credits__ = ["Kris Laratta", ]
 __license__ = "GPL"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __maintainer__ = "Kris Laratta"
 __email__ = "krislaratta@gmail.com"
 __status__ = "Development"
@@ -34,6 +35,34 @@ class Lovelight():
         for l in self.lights:
             l.light.on = False
 
+    @contextmanager
+    def graceful_stop(self,
+                      light,
+                      end_state,
+                      brightness=None,
+                      transition_time=None):
+        """
+        Allows the user to define a state for a routine to end on.
+        """
+        try:
+            yield
+        finally:
+            if end_state == "off":
+                light.off()
+            elif end_state == "on":
+                light.on()
+            elif end_state == "dim":
+                if brightness and transition_time:
+                    light.dim(False, brightness, transition_time)
+                elif brightness:
+                    light.dim(False, brightness, 50)
+                elif transition_time:
+                    light.dim(False, 0, transition_time)
+                else:
+                    light.dim(False, 0, 50)
+            else:
+                raise NotImplementedError("Not a command.")
+
 
 class Light():
     def __init__(self, hue_light, bridge):
@@ -54,8 +83,7 @@ class Light():
     def off(self):
         self.light.on = False
 
-    def dim(self, end_state, brightness, seconds):
-        transition_time = seconds * 10
+    def dim(self, end_state, brightness, transition_time):
         self.bridge.set_light(
             self.id,
             {
